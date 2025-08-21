a1471219ac1c484bccc751a3b26625d5b2c2f4a1
vachikdavtyan-d <vachikdartyan@gmail.com>
2025-08-21 12:38:54 +0200

    Create impressum.js
    Signed-off-by: vachikdavtyan-d <vachikdartyan@gmail.com>

---
+++ pages/impressum.js
@@ -0,0 +1,18 @@
+export default function Impressum() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900 p-10">
+      <h1 className="text-3xl font-bold mb-6">Impressum</h1>
+      <p>Muster Kurier & Umzüge</p>
+      <p>Musterstraße 123</p>
+      <p>12345 Hamburg</p>
+      <p>Telefon: 01234 56789</p>
+      <p>E-Mail: info@muster-kurier.de</p>
+      <br />
+      <p>Geschäftsführer: Max Mustermann</p>
+      <p>Handelsregister: Amtsgericht Hamburg HRB 123456</p>
+      <p>USt-IdNr.: DE123456789</p>
+      <br />
+      <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: Max Mustermann</p>
+    </div>
+  );
+}
